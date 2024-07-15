10aeb5f8df5f1ec1ed9b008eda974a499d666b31
sumama <sumamakhan761>
2024-07-15 15:48:40 +0530

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,15 +18,15 @@ app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use("/api/v1/users", userRoutes)
 
-
-const start = async() => {
+const start = async () => {
   app.set("mongo_user")
   const connectiononDb = await mongoose.connect("mongodb+srv://sumamakhan800:yJJZfUsrYogARA0M@cluster0.huz6uqk.mongodb.net/")
 
   console.log(`MONGO Connected DB Host: ${connectiononDb.connection.host}`)
 
-  app.listen(app.get("port"),()=>{
+  app.listen(app.get("port"), () => {
     console.log("LISTENING ON PORT 8000")
   });
 
